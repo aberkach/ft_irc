fe4dfe6eaa8a519920367fc96bdef702f12cc4b8
Abdelfattah Berkach <abberkac@e1r3p5.1337.ma>
2024-03-26 22:30:25 +0000

    the server in a loop, and accept 1 client , the clien can send msg by the first argument

--- testServerClient/clientTest.c
+++ testServerClient/clientTest.c
@@ -6,64 +6,50 @@
 /*   By: abberkac <abberkac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 23:55:52 by abberkac          #+#    #+#             */
-/*   Updated: 2024/03/26 00:16:11 by abberkac         ###   ########.fr       */
+/*   Updated: 2024/03/26 22:28:28 by abberkac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// provides required data types
+#include <sys/_endian.h>
 #include<sys/types.h>
-// has address families and socket functions
 #include<sys/socket.h>
-// has the sockaddr_in structure
 #include<netinet/in.h>
-// header for reading and write functions
 #include<fcntl.h>
 #include <stdio.h>
-// header to help with strings
 #include <string.h>
-// header for close function
 #include <unistd.h>
-// has macros such as EXIT_FAILURE
 #include<stdlib.h>
-// port through which connection is to be made
 #define CONNECTION_PORT 3500
 
-int main()
+int main(int ac, char **av)
 {
-// structure to represent the address
     struct sockaddr_in server_address;
-// Message to be sent to the server
-    char* message = "This is a message from the client";
-// storage buffer to receive messages
+    if (ac != 2)
+    {
+        printf("Usage: %s <message>\n", av[0]);
+        exit(EXIT_FAILURE);
+    }
+    char* message = av[1];
     char recieve_buffer[100];
-// create an Ipv4 and TCP socket
-    int socket_descriptor = socket(AF_INET,SOCK_STREAM,0);
-// Check whether the socket is created successfully
+    int socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(socket_descriptor < 0)
    {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
    }  
     int status = 0;
-// initialize address structure for binding
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(CONNECTION_PORT);
-// set address to any address available
-   server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_zero[7] ='\0';
-// connect to the server
+   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     status = connect(socket_descriptor,(struct sockaddr*)&server_address, sizeof(server_address));
     if(status < 0)
    {
         perror("Couldn't connect with the server");
         exit(EXIT_FAILURE);
    }    
-// send message to the server
     write(socket_descriptor, message, strlen(message));
-//receive a message from the server
     read(socket_descriptor, recieve_buffer, 100);
     printf("Message from server: %s\n", recieve_buffer);
-// terminate the socket connection
     close(socket_descriptor);
     return 0;
 }
--- testServerClient/serverTest.c
+++ testServerClient/serverTest.c
@@ -6,94 +6,80 @@
 /*   By: abberkac <abberkac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 23:48:46 by abberkac          #+#    #+#             */
-/*   Updated: 2024/03/26 00:20:02 by abberkac         ###   ########.fr       */
+/*   Updated: 2024/03/26 22:28:04 by abberkac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// provides required data types
+#include <sys/_endian.h>
 #include <sys/types.h>
-// holds address family and socket functions
 #include <sys/socket.h>
-// has the sockaddr_in structure
 #include <netinet/in.h>
-// has functions for read and write operations
 #include <fcntl.h>
-// basic C header
 #include <stdio.h>
-// header to help with strings
 #include <string.h>
 #include <unistd.h>
-// has macros such as EXIT_FAILURE
 #include<stdlib.h>
-// port through which connection is to be made
 #define CONNECTION_PORT 3500
 
 int main()
 {
-// server socket 
     int socket_descriptor;
-// socket created by the bind function
     int client_socket;
-// buffer to store message
     char storage_buffer[80];
-    unsigned int length_of_address;
-// option value for respective option_name
     int option_value = 1;
-// server and client address structures
     struct sockaddr_in server_address;
-    struct sockaddr_in connection_address;
+    
     char* message = "This is a message from the server";
-// creating the socket with IPv4 domain and TCP protocol
-    socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-// Check if the socket is created successfully
+    socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (socket_descriptor < 0)
    {
         perror("Socket creation failed");
-// EXIT_FAILURE is a macro used to indicate unsuccessful execution of the program
         exit(EXIT_FAILURE);
    }     
-// set options for the socket
     int status = setsockopt(socket_descriptor, SOL_SOCKET,SO_REUSEADDR , &option_value,sizeof(option_value));
-// check if options are set successfully
     if (status  < 0){
         perror("Couldn't set options");
         exit(EXIT_FAILURE);
     }
-//  initializing structure elements for address
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(CONNECTION_PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_zero[7] = '\0';
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     
     status = bind(socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
     if (status < 0){
         perror("Couldn't bind socket");
         exit(EXIT_FAILURE);
     }
-// listen on specified port with a maximum of 4 requests
     status = listen(socket_descriptor,4);
-// check if the socket is listening successfully
     if (status < 0){
         perror("Couldn't listen for connections");
         exit(EXIT_FAILURE);
     }
-    length_of_address = sizeof(connection_address);
-// accept connection signals from the client
-    client_socket = accept(socket_descriptor, (struct sockaddr*)&connection_address, &length_of_address);
-// check if the server is accepting the signals from the client
-    if (client_socket < 0){
-        perror("Couldn't establish connection with client");
-        exit(EXIT_FAILURE);
+    while (1)
+    {
+        struct sockaddr_in connection_address;
+        unsigned int length_of_address;
+
+        printf("Server is listening for connections\n");
+        client_socket = accept(socket_descriptor, (struct sockaddr*)&connection_address, &length_of_address);
+        if (client_socket < 0){
+            perror("Couldn't establish connection with client");
+            exit(EXIT_FAILURE);
+        }
+        int bytes_read;
+
+        while ((bytes_read = read(client_socket, storage_buffer, 79)) > 0) {
+            
+            storage_buffer[bytes_read] = '\0';
+            printf("Message from client: %s \n",storage_buffer);
+            send(client_socket, message, strlen(message), 0);
+            
+            if (bytes_read < 0){
+                perror("Couldn't read from client");
+                exit(EXIT_FAILURE);
+            }
+        }
     }
-// Receive data sent by the client
-    int bytes_read = read(client_socket, storage_buffer, 80);
-// set the last index of the character array as a null character
-    storage_buffer[bytes_read] = '\0';
-    printf("Message from client: %s \n",storage_buffer);
-// Send data to the client
-    send(client_socket, message, strlen(message), 0);
-    
-// close the socket
     close(socket_descriptor);
     close(client_socket);
     return 0;
