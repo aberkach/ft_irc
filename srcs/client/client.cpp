ad7d5dc40366e6efb7410764dfc29dae7111a0ca
ElmehdiBennix <bennixmehdi@gmail.com>
2024-04-02 21:48:48 +0000

    MAKE_PUSH

--- srcs/client/client.cpp
+++ srcs/client/client.cpp
@@ -1,14 +1,37 @@
 #include "client.hpp"
 
 
-Client::Client(int sock) : socket(sock) {
-    nickname = "default";
-    username = "default";
-    realname = "default";
-}
+Client::Client(int sock) : socket(sock) , nickname(""), username(""), realname("") {};
 
-Client::Client(std::string nick, std::string user, std::string real) {
-    nickname = nick;
-    username = user;
-    realname = real;
-}
+void Client::setNickname(const std::string& nick) 
+{
+     nickname = nick; 
+};
+
+void Client::setUsername(const std::string& user) 
+{
+     username = user; 
+};
+
+void Client::setRealname(const std::string& real) 
+{
+     realname = real; 
+};
+
+
+std::string Client::getNickname() const
+{
+    return nickname;
+};
+
+std::string Client::getUsername() const
+{
+    return username;
+};
+
+std::string Client::getRealname() const
+{
+    return realname;
+};
+
+Client::~Client(){};
