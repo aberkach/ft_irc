dbb280582aa98ae9af8fb5db9299533295c3e736
Abdelfattah Berkach <abberkac@e3r10p13.1337.ma>
2024-07-24 06:04:46 +0100

    merger the bot

---
+++ bonus/bot/bot.hpp
@@ -0,0 +1,48 @@
+#ifndef BOT_HPP__
+#define BOT_HPP__
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+class Bot {
+    private:
+        int irc_sock;
+
+        std::string API_KEY;
+
+        std::vector<std::string> masters;
+        std::vector<std::string> channels;
+
+        typedef void (Bot::*commandFunc) (const std::vector<std::string>&);
+        std::map<std::string, commandFunc> commandList;
+
+    private:
+        void            logger(const std::string &message) const;
+        bool            masterStatus(const std::string &name);
+
+        void            pongCommand(const std::vector<std::string> &fields);
+        void            welcomeMsg(const std::vector<std::string> &fields);
+        void            leaveMsg(const std::vector<std::string> &fields);
+        void            helpCommand(const std::vector<std::string> &fields);
+        void            jokeCommand(const std::vector<std::string> &fields);
+        void            tossCommand(const std::vector<std::string> &fields);
+        void            weatherCommand(const std::vector<std::string> &fields);
+        // void         pollCommand(const std::vector<std::string> &fields);
+        void            adminCommand(const std::vector<std::string> &fields);
+
+        const std::map<std::string, std::string>    extractWeatherData(const std::string &response);
+        std::string                                 getWeatherData(const std::map<std::string, std::string>& weatherData , const std::string &client);
+
+
+    public:
+        Bot(void);
+
+        void startBot(uint16_t port);
+        void authenticate(std::string password) const;
+        void listenForCommand(void);
+
+        ~Bot();
+};
+
+#endif
---
+++ bonus/main.cpp
@@ -0,0 +1,21 @@
+#include "include/ircBot.hpp"
+
+int main(int ac, char **av) {
+    uint16_t port;
+
+    if ((port = arg_checker(ac ,av)) != 1)
+    {
+        try {
+            Bot agent;
+
+            agent.startBot(port);
+            agent.authenticate(av[2]);
+            agent.listenForCommand();
+
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            return -10;
+        }
+    }
+    return port;
+};
