a61a729da9ad102ef3638cfb4d096502a2d91636
rambeau <bennixmehdi0@gmail.com>
2024-08-07 20:30:55 +0100

    MAKE_PUSH

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -19,8 +19,8 @@ int main(int ac, char **av)
     uint16_t port;
     if ((port = health::arg_checker(ac ,av)) != 1)
     {
-        Server srv(port, av[2]);
         try {
+            Server srv(port, av[2]);
             srv.createServer();
         } 
         catch (std::exception &e) {
